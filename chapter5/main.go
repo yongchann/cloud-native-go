0b59bd17a8f73424a323f53ab706455e98781815
yongchann <yongcgp@naver.com>
2024-03-02 02:44:41 +0900

    [ch5] (#2)add simple store rest api

---
+++ chapter5/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+)
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	key := mux.Vars(r)["key"]
+
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if err := Put(key, string(value)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	key := mux.Vars(r)["key"]
+
+	if err := Delete(key); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	key := mux.Vars(r)["key"]
+
+	value, err := Get(key)
+	if err != nil {
+		if errors.Is(err, ErrorNoSuchKey) {
+			http.NotFound(w, r)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	w.Write([]byte(value))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods(http.MethodPut)
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods(http.MethodDelete)
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cloud-native-go-study
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.1 // indirect
