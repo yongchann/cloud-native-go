77d912e659e648181e4556f9caa29c8faa6ab49c
yongchann <yongcgp@naver.com>
2024-03-02 01:46:26 +0900

    [ch5] (#1)add tiny store

---
+++ chapter5/store.go
@@ -0,0 +1,28 @@
+package main
+
+import "errors"
+
+var store = make(map[string]string)
+
+var ErrorNoSuchKey = errors.New("no such key")
+
+func Put(key string, value string) error {
+	store[key] = value
+
+	return nil
+}
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+
+	return nil
+}
